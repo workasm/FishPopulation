aafed8f6da5f7f8ed954be51f5822ca541c484d1
Pavel Emeliyanenko <p.emeliyanenko@precitec-optronik.de>
2023-06-13 10:40:01 +0200

    updated fish sim

--- LambdaSimulation.cpp
+++ LambdaSimulation.cpp
@@ -1,6 +1,8 @@
 
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <math.h>
 #include "omp.h"
@@ -51,12 +53,16 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
         Real sum = std::numeric_limits< Real >::max();
     };
 
-    constexpr int nmins = 500;
+    // N(beta0) = N(Mw1) + N(lambda2) + N(lambda1)
+    // N(lambda2) =N(lambda1)*beta1/lambda1
+
+
+    constexpr int nmins = 50;
     std::vector<Params> g_mins;
 
     omp_set_num_threads(8);
 
-    constexpr Real step = 0.003;
+    constexpr Real step = 0.0005;
     constexpr Real lambdaMin = 0.02, lambdaMax = 0.98;
     constexpr Real MpMin = 0.02, MpMax = 0.98;
     constexpr Real MwMin = 0.02, MwMax = 0.98;
@@ -84,21 +90,34 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
         fprintf(stderr, "thid: %d; beta0: %.3f -- num: %d\n", omp_get_thread_num(), cur.beta0, i);
 
     //for(cur.lambda0 = 0.24; cur.lambda0 <= 0.24; cur.lambda0 += step)
+
+    // original:
+    // Mp + Mw0 + beta0 + lambda0 = 1
+    // Mw1 + beta1 + lambda1 = 1
+    // N(lambda2) = N(lambda1)*beta1/lambda1
+    // N(lambda1)/lambda1 = N(lambda0)*beta0/lambda0
+
+    // extra: lambda0 < lambda1
+    // Mw0*5 = Mw1
+
+    // N(lambda0)*beta0/lambda0 = N(Mw1) + N(lambda1)*beta1/lambda1 + N(lambda1)
+
+    //Real beta0 = 0, lambda0 = 0, Mw0 = 0;
+    //Real beta1 = 0, Mw1 = 0;
+    //Real sum = std::numeric_limits< Real >::max();
+
     {
-        Real Mp = 1.0 - cur.beta0 - cur.lambda0;
+    for(cur.Mw0 = MwMin; cur.Mw0 <= MwMax; cur.Mw0 += step)
+    {
+        Real Mp = 1.0 - cur.beta0 - cur.lambda0 - cur.Mw0;
         if(!(Mp >= MpMin && Mp <= MpMax))
             continue;
 
-    for(cur.Mw0 = MwMin; cur.Mw0 <= MwMax; cur.Mw0 += step)
-    {
-        for(cur.Mw1 = MwMin; cur.Mw1 <= MwMax; cur.Mw1 += step) {
-
-        if(!(cur.Mw0 >= cur.Mw1*MwFactor))
-            continue;
+        cur.Mw1 = cur.Mw0 / MwFactor;
 
         for(cur.beta1 = step; cur.beta1 <= 1.0 - step; cur.beta1 += step) {
 
-            Real lambda1 = 1.0 - cur.beta1;
+            Real lambda1 = 1.0 - cur.beta1 - cur.Mw1;
             if(!(cur.lambda0 + lambdaMinDiff < lambda1 &&
                  lambda1 >= lambdaMin && lambda1 <= lambdaMax))
                 continue;
@@ -106,11 +125,10 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
             if(!(cur.beta0 > cur.beta1 + betaMinDiff))
                 continue;
 
-            auto beta0 = cur.beta0*(1.0 - cur.Mw0), beta1 = cur.beta1*(1.0 - cur.Mw1);
-            auto val1 = opts.Nout0*beta0*lambda1 - opts.Nout1*cur.lambda0,
-                    val2 = opts.Nout1*beta1 - opts.Nout2*lambda1;
+            auto val1 = opts.Nout0*cur.beta0*lambda1 - opts.Nout1*cur.lambda0,
+                 val2 = opts.Nout1*cur.beta1 - opts.Nout2*lambda1;
 
-            cur.sum = std::abs((val1)) + std::abs((val2));
+            cur.sum = std::abs(val1) + std::abs(val2);
 
             for(int i = 0; i < nmins; i++) {
                 if(mins[i].sum > cur.sum) {
@@ -123,7 +141,6 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
                 }
             }
         }
-    }
     }
     }
     } // for beta0
@@ -142,7 +159,7 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
 
     int i = 1; (void)i;
     std::sort(g_mins.begin(), g_mins.end(), [](const Params& p1, const Params& p2){
-        return p1.beta0*(1.0 - p1.Mw0) < p2.beta0*(1.0 - p2.Mw0);
+        return p1.beta0 < p2.beta0;
     });
 
     struct Range {
@@ -168,35 +185,30 @@ void runLambdaInternal(std::ofstream& ofs, const FishAgesNum& opts, Real lambda0
 
     char bbf[256];
     for(const Params& item: g_mins) {
-        Real lambda1 = 1.0 - item.beta1;
+        Real lambda1 = 1.0 - item.beta1 - item.Mw1;
 
-        auto beta0 = item.beta0*(1.0 - item.Mw0),
-             beta1 = item.beta1*(1.0 - item.Mw1);
-        auto val1 = std::abs(opts.Nout0*beta0/item.lambda0 - opts.Nout1/lambda1),
-             val2 = std::abs(opts.Nout1*beta1/lambda1 - opts.Nout2);
+        auto val1 = std::abs(opts.Nout0*item.beta0/item.lambda0 - opts.Nout1/lambda1),
+             val2 = std::abs(opts.Nout1*item.beta1/lambda1 - opts.Nout2);
 
-        auto Mw0 = item.Mw0*item.beta0,
-             Mw1 = item.Mw1*item.beta1;
+        auto Mp0 = 1.0 - item.beta0 - item.lambda0 - item.Mw0;
 
-        auto MpMw0 = 1.0 - beta0 - lambda0;
-
-        if(beta0 == 0.0)
+        if(item.beta0 == 0.0)
             continue;
 
-        if(val1 > 600 || val2 > 600)
-            continue;
+        //if(val1 > 600 || val2 > 600)
+         //   continue;
 
-        setMM(beta0R, beta0);
-        setMM(beta1R, beta1);
-        setMM(Mw0R, Mw0);
-        setMM(Mw1R, Mw1);
+        setMM(beta0R, item.beta0);
+        setMM(beta1R, item.beta1);
+        setMM(Mw0R, item.Mw0);
+        setMM(Mw1R, item.Mw1);
         setMM(lambda0R, item.lambda0);
         setMM(lambda1R, lambda1);
         setMM(val1R, val1);
         setMM(val2R, val2);
 
-        sprintf(bbf, "%d ; %.5f ; %.5f ; %.5f / %.5f ; %.5f ; %.5f ; %.5f ; %.2f ; %.2f\n",
-            i++, beta0, item.lambda0, MpMw0, Mw0, beta1, lambda1, Mw1, std::abs(val1), std::abs(val2));
+        sprintf(bbf, "%d ; %.5f ; %.5f ; %.5f ; %.5f ; %.5f ; %.5f ; %.5f ; %.2f ; %.2f\n",
+            i++, item.beta0, item.lambda0, Mp0, item.Mw0, item.beta1, lambda1, item.Mw1, val1, val2);
         ofs << bbf;
     }
 }
@@ -206,13 +218,13 @@ void runLambda() {
     //constexpr int64_t shift = 26, mod = 1<<shift;
     // this is a group I
     FishAgesNum groupI = {
-        48225, 6746, 2624
+        48207, 6743, 2621
     };
     FishAgesNum groupII = {
-        9241, 2836, 1103
+        9339, 2833, 1103
     };
 
-    constexpr int64_t numFishMin = 475, numFishMax = 475, fishStep = 50;
+    int64_t numFishArr[] = { 190, 210, 260, 310, 340, 390, 410 };
     constexpr Real numFishEggs = 1100*0.95;
 
     const Real factorI = 0.85,
@@ -228,31 +240,23 @@ void runLambda() {
 //    ofs << "GroupII; Nout0=" << groupII.Nout0 << "; Nout1=" << groupII.Nout1 << "; Nout2=" << groupII.Nout2 << "\n";
 //    ofs << "FactorI=" << factorI << "; FactorII=" << factorII << "\n";
 
-    //ofs << "No ; beta0*(1-Mw0) ; lambda0 ; Mp; Mw0 ; beta1*(1-Mw1) ; lambda1 ; Mw1 ; |A1-A2| ; |B1-B2|\n";
-    //ofs << "No ; beta0*(1-Mw0) ; lambda0 ; Mp; beta0*Mw0 ; beta1*(1-Mw1) ; lambda1 ; beta1*Mw1 ; |A1-A2| ; |B1-B2|\n";
-
-//    ofs << "No; beta0*(1-Mw0) ; lambda0 ; beta0*Mw0 ; beta1*(1-Mw1) ; lambda1 ; beta1*Mw1 ; |A1-A2| ; |B1-B2|\n";
-
-    for(int64_t numFish = numFishMin; numFish <= numFishMax; numFish += fishStep) {
+    for(auto numFish : numFishArr) {
 
         Real total0 = numFish*numFishEggs;
         Real lambda0_I = groupI.Nout0 / (total0 * factorI),
              lambda0_II = groupII.Nout0 / (total0 * factorII);
 
         {
-        std::ofstream ofs("C:\\work\\LambdaSim_Fish" + std::to_string(numFish) + "_I.csv");
+        std::ofstream ofs("C:/work/LambdaSim_Fish" + std::to_string(numFish) + "_I.csv");
         fprintf(stderr, "###### numFish = %lld #######\n", numFish);
-         ofs << "No; beta0*(1-Mw0) ; lambda0 ; beta0*Mw0 ; beta1*(1-Mw1) ; lambda1 ; beta1*Mw1 ; |A1-A2| ; |B1-B2|\n";
-        //ofs << "NumFish=" << numFish << "\n";
-        //ofs << "Group I\n";
+        ofs << "No; beta0 ; lambda0 ; Mp0; Mw0 ; beta1 ; lambda1 ; Mw1 ; |A1-A2| ; |B1-B2|\n";
         runLambdaInternal(ofs, groupI, lambda0_I, true);
         }
 
         {
         //ofs << "####### ; ########; #######;########;########;###########################\n";
-        std::ofstream ofs("C:\\work\\LambdaSim_Fish" + std::to_string(numFish) + "_II.csv");
-         ofs << "No; beta0*(1-Mw0) ; lambda0 ; beta0*Mw0 ; beta1*(1-Mw1) ; lambda1 ; beta1*Mw1 ; |A1-A2| ; |B1-B2|\n";
-        //ofs << "Group II\n";
+        std::ofstream ofs("C:/work/LambdaSim_Fish" + std::to_string(numFish) + "_II.csv");
+         ofs << "No; beta0 ; lambda0 ; Mp0; Mw0 ; beta1 ; lambda1 ; Mw1 ; |A1-A2| ; |B1-B2|\n";
         runLambdaInternal(ofs, groupII, lambda0_II, false);
         }
 
